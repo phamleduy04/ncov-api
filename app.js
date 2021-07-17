745fe117d604f806ed8132be1e753e3997d1cdd6
Duy Pham Le <phamleduy04@gmail.com>
2021-07-16 20:52:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -46,8 +46,10 @@ app.use((err, req, res, next) => {
 const ms = require('ms');
 const { executeBigScraper, executeSmallScraper } = require('./handlers/instances');
 
-executeSmallScraper();
-executeBigScraper();
+if (process.env.NODE_ENV != 'dev') {
+  executeSmallScraper();
+  executeBigScraper();
+};
 
 setInterval(executeBigScraper, ms('5h'));
 setInterval(executeSmallScraper, ms('10m'));
