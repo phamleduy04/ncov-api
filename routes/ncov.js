9dba7a37c0c88657d0db8f826ef16d42fb7c52ad
Duy Pham Le <phamleduy04@gmail.com>
2021-07-25 23:19:06 -0500

    typo again :(

--- routes/ncov.js
+++ routes/ncov.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 // const { get } = require('../handlers/database');
 
 router.get('/', async (req, res) => {
-    return res.json({ error: true, message: 'desperated' });
+    return res.json({ error: true, message: 'Deprecated' });
 });
 
 router.get('/:patientid', async (req, res) => {
-    return res.json({ error: true, message: 'desperated' });
+    return res.json({ error: true, message: 'Deprecated' });
 });
 
 module.exports = router;
