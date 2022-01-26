8b1334c4b55ed468191fbdb39825f9510337126d
Duy Pham Le <phamleduy04@gmail.com>
2022-01-26 16:42:12 -0600

    Update database.js

--- handlers/database.js
+++ handlers/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const { Database } = require("quickmongo");
 const db = new Database(process.env.MONGODB || 'mongodb://localhost/ncov-api');
-db.connect().then(() => console.log('Database is ready!'));
 
+db.on('ready', () => console.log('Database is ready!'));
 module.exports = {
     get: async function(key) {
         if (!key) throw new Error('Key is null!');
