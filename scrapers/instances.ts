bf4c51c8e1595ecffbdeabfd3a65e7b17e5df119
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2022-05-03 15:36:26 -0500

    add cityvn scraper

--- scrapers/instances.ts
+++ scrapers/instances.ts
@@ -1,10 +1,12 @@
 import getWOM from './wom.world';
 import getStates from './wom.usstate';
+import getCityVN from './cityvn';
 
 const execute = async (): Promise<void> => {
     await Promise.all([
         getWOM(),
         getStates(),
+        getCityVN(),
     ]);
 };
 export default execute;
