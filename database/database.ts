c6946729eb32447a03ba5746978d3258902373f6
Duy Pham Le <32657584+phamleduy04@users.noreply.github.com>
2023-03-04 22:30:52 -0600

    simplify database

--- database/database.ts
+++ database/database.ts
@@ -1,15 +1,9 @@
-import { MongoClient } from 'mongodb';
-import { Collection } from './collection';
+import * as Keyv from 'keyv';
 import 'dotenv/config';
-import { info } from '../utils/log';
+import { error } from '../utils/log';
 
-
-const mongo: MongoClient = new MongoClient(process.env.MONGODB || "mongodb://localhost/economy");
-mongo.connect().then(() => info("Connected to MongoDB"));
-
-const mongoCollection = mongo.db().collection('ncov');
-const db = new Collection(mongoCollection, mongo.startSession());
-
+const db = new Keyv(process.env.DBURL || undefined);
+db.on('error', err => error('Connection Error', err));
 
 const get = async (key: string) => {
     if (!key) throw new Error('No key provided');
@@ -21,22 +15,7 @@ const set = async (key: string, value: any) => {
     return await db.set(key, value);
 };
 
-const getAll = async () => {
-    return await db.all();
-};
-
-const reset = async () => {
-    return await db.drop();
-};
-
-const close = async () => {
-    return await mongo.close();
-};
-
 export {
     get,
     set,
-    getAll,
-    reset,
-    close,
 };
